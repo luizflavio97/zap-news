e818b1374d39cc3fc1b2a367f8726cc8e2233d85
Luiz Flávio <luizflavio_97@hotmail.com>
2025-12-19 21:18:10 +0000

    finalizar milestone relacionada ao db local

---
+++ infra/database.js
@@ -0,0 +1,20 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  });
+
+  await client.connect();
+  const res = await client.query(queryObject);
+
+  return res;
+}
+
+export default {
+  query: query,
+};
