9c8f94b40ec0b592aa7eb2c4d636900188096093
Luiz Flávio <luizflavio_97@hotmail.com>
2026-01-14 20:10:22 +0000

    missing migrations client configuration

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,12 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
-export default async function migrations(req, res) {
-  const defaultMigrationsOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
@@ -11,23 +14,26 @@ export default async function migrations(req, res) {
     migrationsTable: "pgmigrations",
   };
 
-  if (req.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-    res.status(200).json(pendingMigrations);
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
   }
 
-  if (req.method === "POST") {
+  if (request.method === "POST") {
     const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
+      ...defaultMigrationOptions,
       dryRun: false,
     });
 
+    await dbClient.end();
+
     if (migratedMigrations.length > 0) {
-      return res.status(201).json(migratedMigrations);
+      return response.status(201).json(migratedMigrations);
     }
 
-    return res.status(200).json(migratedMigrations);
+    return response.status(200).json(migratedMigrations);
   }
 
-  return res.status(405);
+  return response.status(405).end();
 }
