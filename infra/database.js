1421537c0a0714d77e17abaf5254298859f68bf3
Luiz Flávio <luizflavio_97@hotmail.com>
2026-01-14 20:04:12 +0000

    adds `api/v1/migration` endpoints

--- infra/database.js
+++ infra/database.js
@@ -1,28 +1,34 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-
+  let client;
   try {
-    await client.connect();
-    const res = await client.query(queryObject);
-
-    return res;
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
   } catch (error) {
-    console.log(error);
+    console.error(error);
     throw error;
   } finally {
     await client.end();
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,33 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+export default async function migrations(req, res) {
+  const defaultMigrationsOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (req.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    res.status(200).json(pendingMigrations);
+  }
+
+  if (req.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+
+    if (migratedMigrations.length > 0) {
+      return res.status(201).json(migratedMigrations);
+    }
+
+    return res.status(200).json(migratedMigrations);
+  }
+
+  return res.status(405);
+}
