588aaa40695f7e82e514b2a89e78f73e7aa83fdf
Luiz Flávio <luizflavio_97@hotmail.com>
2026-01-22 22:16:57 +0000

    add `orchestrator.js` to handle race condition on tests

---
+++ tests/orchestrator.js
@@ -0,0 +1,18 @@
+import retry from "async-retry";
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+    });
+
+    async function fetchStatusPage() {
+      const res = await fetch("http://localhost:3000/api/v1/status");
+      const resBody = await res.json();
+    }
+  }
+}
+
+export default { waitForAllServices };
