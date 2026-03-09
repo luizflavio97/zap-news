1e799c836a287de76b9c554dbd4064d70a8ad097
Luiz Flávio <luizflavio_97@hotmail.com>
2026-03-09 19:09:27 +0000

    feat: add custom errors initial implementation

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,35 +1,46 @@
 import database from "/infra/database.js";
+import { InternalServerError } from "/infra/errors.js";
 
 async function status(req, res) {
-  const updatedAt = new Date().toISOString();
-  const databaseVersion = await database
-    .query("SHOW server_version;")
-    .then((result) => {
-      return result.rows[0].server_version;
+  try {
+    const updatedAt = new Date().toISOString();
+    const databaseVersion = await database
+      .query("SHOW server_version;")
+      .then((result) => {
+        return result.rows[0].server_version;
+      });
+
+    const maxConnections = await database
+      .query("SHOW max_connections;")
+      .then((result) => {
+        return result.rows[0].max_connections;
+      });
+
+    const databaseName = process.env.POSTGRES_DB;
+    const activeConnections = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
     });
 
-  const maxConnections = await database
-    .query("SHOW max_connections;")
-    .then((result) => {
-      return result.rows[0].max_connections;
-    });
-
-  const databaseName = process.env.POSTGRES_DB;
-  const activeConnections = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName],
-  });
-
-  res.status(200).send({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        db_version: databaseVersion,
-        max_connections: parseInt(maxConnections),
-        active_connections: activeConnections.rows[0].count,
+    res.status(200).send({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          db_version: databaseVersion,
+          max_connections: parseInt(maxConnections),
+          active_connections: activeConnections.rows[0].count,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("Controller Error");
+    console.error(publicErrorObject);
+    res.status(500).send(publicErrorObject);
+  }
 }
 
 export default status;
