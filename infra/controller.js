446bec49cccddc74a0dc385024a3a0e8438b64e7
Luiz Flávio <luizflavio_97@hotmail.com>
2026-03-17 00:01:23 +0000

    feat: create and user `DataValidationError` in `controller.js`

--- infra/controller.js
+++ infra/controller.js
@@ -1,6 +1,15 @@
-import { InternalServerError, MethodNotAllowed } from "/infra/errors.js";
+import {
+  InternalServerError,
+  MethodNotAllowed,
+  DataValidationError,
+} from "/infra/errors.js";
 
 function onErrorHandler(err, req, res) {
+  if (err instanceof DataValidationError) {
+    res.status(err.statusCode).json(err);
+    return;
+  }
+
   const publicErrorObject = new InternalServerError({
     cause: err,
     statusCode: err.statusCode,
